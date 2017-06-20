8d13a140819a5a081b5fb57ae7339896c6ff14b6
Tim Neville <nikefootbag@gmail.com>
2017-06-21 09:48:51 +1000

    Renamed CircularlyLinkedList Class and Test to LinkedListInheritance Class and Test

---
+++ tests/com/timneville/LinkedListInheritanceTest.java
@@ -0,0 +1,38 @@
+package com.timneville;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by timneville on 21/6/17.
+ */
+public class LinkedListInheritanceTest {
+    LinkedListInheritance<String> myList;
+
+    @Before
+    public void setUp() throws Exception {
+        myList = new LinkedListInheritance<String>();
+    }
+
+    @Test
+    public void addNewLink() throws Exception {
+        myList.add("Object 1");
+        myList.add("Object 2");
+        myList.add("Object 3");
+        myList.add("Object 4");
+        myList.add("Object 5");
+
+        assertEquals("Object 1", myList.getFirst());
+        assertEquals("Object 5", myList.getLast());
+        assertEquals("Object 2", myList.get(1));
+        assertEquals("Object 3", myList.get(2));
+        assertEquals("Object 4", myList.get(3));
+        assertEquals("Object 5", myList.get(4));
+        assertEquals(5, myList.size());
+
+    }
+
+
+}
