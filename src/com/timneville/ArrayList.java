c51ae659239f33a0c6db076621ac952136242fba
Tim Neville <nikefootbag@gmail.com>
2017-06-21 17:24:33 +1000

    Added ArrayList class

--- src/com/timneville/ArrayList.java
+++ src/com/timneville/ArrayList.java
@@ -3,6 +3,44 @@ package com.timneville;
 /**
  * Created by timneville on 21/6/17.
  */
-public class ArrayList {
+public class ArrayList<E> {
+    private int size;
+
+    public ArrayList(int size) {
+        this.size = size;
+    }
+
+    public void add(E element) {
+
+    }
+
+    public E remove(int index) {
+        return null;
+    }
+
+    public void remove(E element) {
+
+    }
+
+    public boolean contains(E element) {
+        return false;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public E get(int index) {
+        return null;
+    }
+
+    public int indexOf(E element) {
+        int index = 0;
+        return index;
+    }
 
 }
