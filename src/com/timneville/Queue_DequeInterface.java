f73cdf8639c59ddbcb452a5222afa8407b3ac2f3
Tim Neville <nikefootbag@gmail.com>
2017-07-05 08:56:13 +1000

    Created Deque ADT & DoubleEndedArrayQueue

---
+++ src/com/timneville/Queue_DequeInterface.java
@@ -0,0 +1,23 @@
+package com.timneville;
+
+/**
+ * Created by timneville on 5/7/17.
+ */
+public interface Queue_DequeInterface<E> {
+    /** Returns the number of elements in the deque */
+    int size();
+    /** Returns a boolean indicating whether the deque is empty */
+    boolean isEmpty();
+    /** Returns the first element of the deque, without removing it (or null if empty) */
+    E first();
+    /** Returns the last element of the deque, without removing it (or null if empty) */
+    E last();
+    /** Insert a new element e at the FRONT of the deque*/
+    void addFirst(E element);
+    /** Insert a new element e at the BACK of the deque*/
+    void addLast(E element);
+    /** Remove and return the first element of the deque (or null if empty) */
+    E removeFirst();
+    /** Remove and return the last element of the deque (or null if empty) */
+    E removeLast();
+}
---
+++ src/com/timneville/Queue_DoubleEndedArrayQueue.java
@@ -0,0 +1,79 @@
+package com.timneville;
+
+import java.util.*;
+
+/**
+ * Created by timneville on 5/7/17.
+ */
+public class Queue_DoubleEndedArrayQueue<E> implements Queue_DequeInterface<E> {
+    private static final int CAPACITY = 1000;
+    private int size = 0;
+    private int front = 0;
+    private E[] data;
+
+    public Queue_DoubleEndedArrayQueue() { this(CAPACITY); }
+    public Queue_DoubleEndedArrayQueue(int capacity) {
+        this.data = (E[]) new Object[capacity];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public E first() {
+        if (isEmpty()) { return null; }
+        return data[front];
+    }
+
+    public int lastIndex() {
+        return ((front-1 + size) % data.length);
+    }
+
+    @Override
+    public E last() {
+        if (isEmpty()) { return null; }
+        return data[lastIndex()];
+    }
+
+    @Override
+    public void addFirst(E element) throws IllegalStateException {
+        if (size == data.length) throw new IllegalStateException("Queue is full");
+        int nextSlot = (front + size) % data.length;    //wraps around using modulo
+        data[nextSlot] = element;
+        size++;
+    }
+
+    @Override
+    public void addLast(E element) {
+        if (size == data.length) throw new IllegalStateException(("Queue is full"));
+        int nextSlot = (lastIndex() + 1);
+        data[nextSlot] = element;
+        size++;
+    }
+
+    @Override
+    public E removeFirst() {
+        if (isEmpty()) { return null; }
+        E answer = first();
+        data[front] = null;
+        front = (front + 1) % data.length;              //wraps around using modulo
+        size--;
+        return answer;
+    }
+
+    @Override
+    public E removeLast() {
+        if (isEmpty()) { return null; }
+        E answer = last();
+        data[lastIndex()] = null;
+        size--;
+        return answer;
+    }
+}
