f399fff81a3b74ad5e2b8076daf1a7b2282e2464
Tim Neville <nikefootbag@gmail.com>
2017-07-19 17:19:13 +1000

    ArrayList now dynamically sized with utility resize() method, called from add() method

--- src/com/timneville/Lists/ArrayListImpListADT.java
+++ src/com/timneville/Lists/ArrayListImpListADT.java
@@ -4,11 +4,11 @@ package com.timneville.Lists;
  * Created by timneville on 19/7/17.
  */
 
-/** ArrayList with BOUNDED capacity */
+/** Dynamic ArrayList with ability to resize capacity via resize(int capacity) method */
 public class ArrayListImpListADT<E> implements ListADT<E> {
     //instance variables
-    private static final int CAPACITY = 16;  //bounded capacity
-    private E[] data;                       //data as an array
+    private static final int CAPACITY = 16;  //initial capacity
+    private E[] data;                       //data stored in an array
     private int size = 0;                   //size counter
 
     //constructors
@@ -16,7 +16,7 @@ public class ArrayListImpListADT<E> implements ListADT<E> {
         this(CAPACITY);
     }
     public ArrayListImpListADT(int capacity) {
-        data = (E[]) new Object[capacity]; //safe cast Object Array(super) as an Element Array(sub).
+        data = (E[]) new Object[capacity]; //safe cast Object Array(super) as an E Array(sub).
     }
 
     @Override
@@ -44,10 +44,10 @@ public class ArrayListImpListADT<E> implements ListADT<E> {
     }
 
     @Override /** O(n) worst-case runtime due to having to shift elements */
-    public void add(int atIndex, E element) throws IndexOutOfBoundsException, IllegalStateException {
+    public void add(int atIndex, E element) throws IndexOutOfBoundsException {
         checkIndexInRange(atIndex, size+1);
-        if (size == data.length)                                //check capacity
-            throw new IllegalStateException("Array is full");
+        if (size == data.length)                                //check capacity, if at capacity then call resize() to increase referenced array capacity
+            resize(2 * data.length);
         for (int k = size-1; k >= atIndex; k++)                 //shift elements, starting from list end, toward index
             data[k+1] = data[k];                                //shift left element one to the right.
         data[atIndex] = element;                                //insert new element
@@ -65,10 +65,18 @@ public class ArrayListImpListADT<E> implements ListADT<E> {
         return removedElement;
     }
 
-    //utility method
+    //utility methods
     /** Checks whether the given index is in range [0, n-1] */
     protected void checkIndexInRange(int index, int arraySize) throws IndexOutOfBoundsException {
         if (index < 0 || index >= arraySize)
             throw new IndexOutOfBoundsException("Illegal index: " + index);
+    }
+
+    /** Creates new larger array, copies the data from existing array, then replaces data reference to that of the new larger array */
+    protected void resize(int capacity) {
+        E[] newResizedArray = (E[]) new Object[capacity];       //safe case Object Array as an E array
+        for (int k = 0; k < size; k++)                          //loop through and copy data from original array to new resized array
+            newResizedArray[k] = data[k];
+        data = newResizedArray;                                 //replace original array reference with new array reference.
     }
 }
