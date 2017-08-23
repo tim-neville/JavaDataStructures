e155b51d18adac971361bd975efe81aaacc0c3ae
Tim Neville <nikefootbag@gmail.com>
2017-08-23 15:04:59 +1000

    TreeADT interface added

---
+++ src/com/timneville/Trees/TreeADT.java
@@ -0,0 +1,32 @@
+package com.timneville.Trees;
+
+import com.timneville.Lists.Position;
+
+import java.util.Iterator;
+
+/**
+ * Created by timneville on 23/8/17.
+ */
+public interface TreeADT<E> extends Iterable<E> {
+    Position<E> root();
+
+    Position<E> parent(Position<E> position) throws IllegalArgumentException;
+
+    Iterable<Position<E>> children(Position<E> position) throws IllegalArgumentException;
+
+    int numChildren(Position<E> position) throws IllegalArgumentException;
+
+    boolean isInternal(Position<E> position) throws IllegalArgumentException;
+
+    boolean isExternal(Position<E> position) throws IllegalArgumentException;
+
+    boolean isRoot(Position<E> position) throws IllegalArgumentException;
+
+    int size();
+
+    boolean isEmpty();
+
+    Iterator<E> iterator();
+
+    Iterable<Position<E>> positions();
+}
